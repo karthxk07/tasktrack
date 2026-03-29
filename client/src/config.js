0080312141b82f5574f4dfe9ef4a517849c8148f
karthxk07 <karthikkalyanam07@gmail.com>
2026-03-29 07:57:41 +0530

    fixed env variable inconsitency

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: window._env_?.VITE_API_BASE_URL || 'http://localhost:8080',
+  API_BASE_URL: wwindow._env_?.VITE_APP_PROD_BASE_URL || window._env_?.VITE_APP_DEV_BASE_URL || 'http://localhost:8080',
 };
 
 export default config;
