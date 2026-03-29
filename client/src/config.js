4137e1801a6fdce2442afe9515877160a8bea4bc
karthxk07 <karthikkalyanam07@gmail.com>
2026-03-29 08:01:59 +0530

    fixed a typo

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: window._env_?.VITE_APP__BASE_URL || 'http://localhost:8080',
+  API_BASE_URL: window._env_?.VITE_APP_BASE_URL || 'http://localhost:8080',
 };
 
 export default config;
